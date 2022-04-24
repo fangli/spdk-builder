53b9ffc01ce865fc0d63979f25d34ff65d697d69
Li Fang <fangli@users.noreply.github.com>
2022-04-24 10:22:58 -0700

    Added uring

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ cd spdk
 git apply --ignore-space-change --ignore-whitespace ../patch.diff
 
 ./scripts/pkgdep.sh --rdma
-./configure --with-rdma --enable-lto --disable-tests --disable-unit-tests --disable-examples
+./configure --with-rdma --with-uring --enable-lto --disable-tests --disable-unit-tests --disable-examples
 make -j4
 
 cd ..
